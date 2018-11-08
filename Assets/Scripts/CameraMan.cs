7ed6b453e20e004377b30775c7b1972f2a342bcf
unknown <bacnt@gmail.com>
2018-11-08 11:44:02 +0700

    snake game

---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 temp = Camera.main.transform.position;
+        temp.y += 1;
+        Camera.main.transform.position = temp;
+
+    }
+}
---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+
+    public GameObject linePrefab;
+    GameObject lineGO;
+    Line activeLine;
+
+    /*void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGO = Instantiate(linePrefab);
+            activeLine = lineGO.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
+    }*/
+    private void Start()
+    {
+        lineGO = Instantiate(linePrefab);
+    }
+    void Update()
+    {
+        activeLine = lineGO.GetComponent<Line>();
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Camera.main.transform.position);
+            activeLine.UpdateLine(mousePos);
+        }
+
+    }
+
+}
